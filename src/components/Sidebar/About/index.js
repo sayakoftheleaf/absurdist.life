189917e4973809922e015d67c0850d5a598776fd
wesrer <sayakchatterjee1309@gmail.com>
2018-12-02 23:54:42 +0530

    fixed some app breaking changes

--- src/components/Sidebar/About/index.js
+++ src/components/Sidebar/About/index.js
@@ -9,9 +9,9 @@ class About extends Component {
         <div className="image">
         </div>
         <div className="bio">
-          Software engineer by Profession and Passion. Undergrad at Tufts. Passionate about Cyber Security, Deep learning and Quantum Computing. Reads up on Philosophy from time to time.
+          Software engineer by Profession and Passion. Undergrad at Tufts. Passionate about Cyber Security, Deep learning and Quantum Computing. Reads up on Philosophy and Film Theory from time to time. You can read my musings <a href="https://blog.absurdist.life">here</a>.
           <div className="emoji">
-          👨‍🎓 &nbsp; 🤖 &nbsp; 💻 &nbsp; 🖋️&nbsp; 🏓 &nbsp; 📚 &nbsp;
+          👨‍🎓 &nbsp; 🤖 &nbsp; 💻 &nbsp; 🏓 &nbsp; 📚 &nbsp;
           </div>
         </div>
       </div>
